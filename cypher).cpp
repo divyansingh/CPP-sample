5805ee0ae6fa60decbd7c8a98f93062502da8911
Divyansh Singh <32278644+divyansingh@users.noreply.github.com>
2019-10-09 23:58:52 +0530

    Rename cypher.cpp to cypher).cpp

---
+++ cypher).cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<string.h>
+ 
+int main() {
+	char str[1000],ch='A',ch1='a',ch2,ch3;
+	int k,i,x,y,z,k1;
+	gets(str);
+	scanf("%d",&k);
+	for(i=0;i<strlen(str);i++)
+	{
+	    if(str[i]>='A'  && str[i]<='Z')
+	    {
+	        if((int)str[i]+k>90)
+	        {
+	            x=(int)str[i]+k-90;
+	            printf("%c",ch+x-1);
+	        }
+	        else {
+	   ch3=str[i];
+	        printf("%c",ch3+k);
+	    }
+	}
+	    else
+	    if(str[i]>='a'  && str[i]<='z')
+	    {
+	        if((int)str[i]+k>122)
+	        {
+	            y=(int)str[i]+k-122;
+	            printf("%c",ch1+y);
+	        }
+	        else {
+	            ch2=str[i];
+	        printf("%c",ch2+k);
+	    }
+	    }
+	else
+	if(str[i]>='0'  && str[i]<='9')
+	{
+ 
+	        z=((1*(str[i]-'0'))+k)%10;
+	    printf("%d",z);
+ 
+    }
+	else
+	printf("%c",str[i]);
+	}
+	return 0;
+}
+Language: C++
